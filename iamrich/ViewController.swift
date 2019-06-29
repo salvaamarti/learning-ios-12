73b8c16781863c4d27bafc74edbb22f06c3cd43a
Salva Martí <salvadormartisolsona@gmail.com>
2019-06-29 14:31:23 +0200

    Initial Commit

---
+++ iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iamrich
+//
+//  Created by Salvador Martí Solsona on 29/06/2019.
+//  Copyright © 2019 Salvador Martí Solsona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
